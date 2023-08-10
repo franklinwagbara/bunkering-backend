518cdfcb81f0ad87f16cd1f5258f71113a6ff6a2
dammielair <drayprogrammer45@yahoo.com>
2023-08-10 12:30:50 +0100

    --new commmits

--- Bunkering.Access/Services/StaffService.cs
+++ Bunkering.Access/Services/StaffService.cs
@@ -43,7 +43,7 @@ namespace Bunkering.Access.Services
 				? (allApps.Where(x => x.FADStaffId.Equals(user.Id) && !x.FADApproved && x.Status.Equals(Enum.GetName(typeof(AppStatus), AppStatus.Processing))))
 				: (allApps.Where(x => x.CurrentDeskId.Equals(user.Id)));
 			var permits = await _userManager.IsInRoleAsync(user, Roles.Company) ? await _unitOfWork.Permit.Find(x => x.Application.UserId.Equals(user.Id), "Application") : await _unitOfWork.Permit.GetAll("Application");
-			var facilities = await _userManager.IsInRoleAsync(user, Roles.Company) ? await _unitOfWork.Facility.Find(x => x.CompanyId.Equals(user.CompanyId),"FacilityType") : await _unitOfWork.Facility.GetAll("FacilityType");
+			var facilities = await _userManager.IsInRoleAsync(user, Roles.Company) ? await _unitOfWork.Facility.Find(x => x.CompanyId.Equals(user.CompanyId),"VesselType") : await _unitOfWork.Facility.GetAll("VesselType");
 			var payments = await _userManager.IsInRoleAsync(user, Roles.Company) ? await _unitOfWork.Payment.Find(x => x.Application.UserId.Equals(user.Id), "Application") : await _unitOfWork.Payment.GetAll("Application");
 
             if (user != null)
